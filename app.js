3add70babc22f61a696d3a16bad9eefe3fb63e92
amililime <emilialimadacunha@gmail.com>
2024-03-12 21:37:44 +0000

    add moveGhost

--- app.js
+++ app.js
@@ -167,3 +167,45 @@ const ghosts = [
 ghosts.forEach((ghost) =>
   squares[ghost.currentIndex].classList.add(ghost.className, "ghost")
 );
+
+ghosts.forEach((ghost) => moveGhost(ghost));
+
+function moveGhost(ghost) {
+  const directions = [-1, 1, width, -width];
+  let direction = directions[Math.floor(Math.random() * directions.length)];
+
+  ghost.timerId = setInterval(function () {
+    if (
+      !squares[ghost.currentIndex + direction].classList.contains("ghost") &&
+      !squares[ghost.currentIndex + direction].classList.contains("wall")
+    ) {
+      squares[ghost.currentIndex].classList.remove(
+        ghost.className,
+        "ghost",
+        "scared-ghost"
+      );
+      ghost.currentIndex += direction;
+      squares[ghost.currentIndex].classList.add(ghost.className, "ghost");
+    } else direction = directions[Math.floor(Math.random() * directions.length)];
+
+    if (ghost.isScared) {
+      squares[ghost.currentIndex].classList.add("scared-ghost");
+    }
+    if (
+      ghost.isScared &&
+      squares[ghost.currentIndex].classList.contains("pac-man")
+    ) {
+      ghost.isScared = false;
+      squares[ghost.currentIndex].classList.remove(
+        ghost.className,
+        "ghost",
+        "scared-ghost"
+      );
+      ghost.currentIndex = ghost.startIndex;
+      score += 100;
+      scoreDisplay.innerHTML = score;
+      squares[ghost.currentIndex].classList.add(ghost.className, "ghost");
+    }
+    checkForGameOver();
+  }, ghost.speed);
+}
